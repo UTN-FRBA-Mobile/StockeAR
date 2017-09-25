dc048fa58aca2ae77af3da1c1617ff8f6d54ad8d
Mauro Martinelli <mauro.martinelli@globant.com>
2017-09-25 19:50:51 -0300

    Se crea un proyecto vacio.

---
+++ StockeAR/AppDelegate.swift
@@ -0,0 +1,38 @@
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+    var navigationController: UINavigationController?
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
+        navigationController = UINavigationController()
+        let viewController: ViewController = ViewController()
+        self.navigationController!.pushViewController(viewController, animated: false)
+        
+        self.window = UIWindow(frame: UIScreen.main.bounds)
+        self.window!.rootViewController = navigationController
+        self.window!.makeKeyAndVisible()
+        
+        return true
+    }
+
+    func applicationWillResignActive(_ application: UIApplication) {
+    }
+
+    func applicationDidEnterBackground(_ application: UIApplication) {
+    }
+
+    func applicationWillEnterForeground(_ application: UIApplication) {
+    
+    }
+
+    func applicationDidBecomeActive(_ application: UIApplication) {
+    }
+
+    func applicationWillTerminate(_ application: UIApplication) {
+    }
+    
+}
+
